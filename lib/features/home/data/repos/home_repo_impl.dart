fe2cc02041c45118fd4412f6443d6053e744618d
Menna <mennaragab220@gmail.com>
2024-09-12 22:01:18 +0300

    apiservice & done failure file

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,1 +1,24 @@
+import 'package:books_remake/core/utils/errors/failure.dart';
+import 'package:books_remake/features/home/data/models/book_model/book_model.dart';
+import 'package:books_remake/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
 
+class HomeRepoImpl implements HomeRepo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks() {
+    // TODO: implement fetchSimilarBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchfeaturedBooks() {
+    // TODO: implement fetchfeaturedBooks
+    throw UnimplementedError();
+  }
+}
