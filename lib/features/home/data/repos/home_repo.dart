e70ff1fa8127401750d5e7894e781844d64e5a76
Menna <mennaragab220@gmail.com>
2024-09-12 21:51:31 +0300

    home repo and book model

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,9 @@
+import 'package:books_remake/core/utils/errors/failure.dart';
+import 'package:books_remake/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
+  Future<Either<Failure, List<BookModel>>> fetchfeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks();
+}
